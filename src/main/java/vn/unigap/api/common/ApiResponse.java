61aba8fba42909aa5d867b2150859e28eeee7ac6
Alex Trinh <huy.trinh43@icloud.com>
2023-12-27 12:59:14 -0600

    create api response and PagingInfo.java

---
+++ src/main/java/vn/unigap/api/common/ApiResponse.java
@@ -0,0 +1,53 @@
+package vn.unigap.api.common;
+
+import java.util.List;
+
+public class ApiResponse<T> {
+    private int errorCode;
+    private int statusCode;
+    private String message;
+    private T data;
+    private PagingInfo pagingInfo;
+
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(int errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public PagingInfo getPagingInfo() {
+        return pagingInfo;
+    }
+
+    public void setPagingInfo(PagingInfo pagingInfo) {
+        this.pagingInfo = pagingInfo;
+    }
+}
+
---
+++ src/main/java/vn/unigap/api/common/PagingInfo.java
@@ -0,0 +1,42 @@
+package vn.unigap.api.common;
+
+import java.util.List;
+
+public class PagingInfo {
+    private int page;
+    private int pageSize;
+    private long totalElements;
+    private long totalPages;
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public long getTotalElements() {
+        return totalElements;
+    }
+
+    public void setTotalElements(long totalElements) {
+        this.totalElements = totalElements;
+    }
+
+    public long getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(long totalPages) {
+        this.totalPages = totalPages;
+    }
+}
