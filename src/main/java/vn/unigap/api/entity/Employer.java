400960bed66d23d95855349bf8e2b9bad792b70a
Alex Trinh <huy.trinh43@icloud.com>
2023-12-30 00:40:26 -0600

    add repository unit test

--- src/main/java/vn/unigap/api/entity/Employer.java
+++ src/main/java/vn/unigap/api/entity/Employer.java
@@ -1,11 +1,19 @@
 package vn.unigap.api.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import java.time.LocalDateTime;
 import java.util.Date;
 
 import jakarta.persistence.*;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 @Table(name="Employer")
 public class Employer {
---
+++ src/test/java/vn/unigap/api/repository/EmployerRepositoryTests.java
@@ -0,0 +1,58 @@
+package vn.unigap.api.repository;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import vn.unigap.api.entity.Employer;
+
+import java.util.List;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+public class EmployerRepositoryTests {
+
+    @Autowired
+    private EmployerRepository employerRepository;
+
+    @Test
+    public void EmployerRepository_SaveAll_ReturnSavedEmployer(){
+        Employer employer = Employer.builder()
+                                    .email("hello@gmail.com")
+                                    .name("Hello")
+                                    .province(12345)
+                                    .description("")
+                                    .build();
+
+        Employer saveEmployer = employerRepository.save(employer);
+
+        Assertions.assertThat(saveEmployer).isNotNull();
+        Assertions.assertThat(saveEmployer.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void EmployerRepository_GetAll_ReturnMoreThanOneEmployer(){
+        Employer employer1 = Employer.builder()
+                .email("hello1@gmail.com")
+                .name("Hello1")
+                .province(12345)
+                .description("")
+                .build();
+        Employer employer2 = Employer.builder()
+                .email("hello2@gmail.com")
+                .name("Hello2")
+                .province(12345)
+                .description("")
+                .build();
+        Employer saveEmployer1 = employerRepository.save(employer1);
+        Employer saveEmployer2 = employerRepository.save(employer2);
+
+        List<Employer> employerList = employerRepository.findAll();
+
+        Assertions.assertThat(employerList).isNotNull();
+        Assertions.assertThat(employerList.size()).isEqualTo(2);
+    }
+}
